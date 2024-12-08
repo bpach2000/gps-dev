f337969ede7065824d48e99e0ae5483b1922ce3c
bpach <bpacheco2018@gmail.com>
2024-12-08 16:28:02 -0700

    Updating Framework

---
+++ PythonGoogleMapsAPI/GoogleMapsAPI/GoogleMapsApi/src/MainGoogleMapsApi.py
@@ -0,0 +1,35 @@
+import ElevationMap
+import StaticMapsApi
+import MapTilesApi
+
+# class MainGoogleMapsApi:
+#     # Add Constructor
+#     def __init__(self):
+#         # Initialize the instances of the other classes
+#         self.= Calculator()
+#         self.greeter = Greeter()
+#         self.logger = Logger()
+
+#     # Define a run class to drive program execution
+#     def run(self):
+#         # General example usage for StaticMapsApi
+#         api_key = "AIzaSyAHFnNvs2uAGYs7ur-uETNR-6Kle2VFIIo"
+#         client = googlemaps.Client(api_key)
+
+#         download_static_map(
+#             client,
+#             size=(400, 400),
+#             center=(63.259591, -144.667969),  # Coordinates of the center of the map
+#             zoom=6,
+#             scale=2,
+#             format="png",
+#             maptype="roadmap",
+#             markers=[(62.107733, -145.541936), "Delta+Junction,AK"]
+#         )
+        
+
+# Instantiate the driver and run it
+if __name__ == "__main__":
+    print("Hello")
+    # driver = Driver()
+    # driver.run()
